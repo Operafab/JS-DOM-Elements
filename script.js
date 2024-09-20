78fd3e08331e51a6952ace7d9455c8a8fe166527
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-09-20 15:46:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// createElement
+// removeChild
+// appendChild
+// replaceChild
+// write
+
+
+//Selectors
+document.getElementById
+document.getElementsByClassName
+document.getElementsByName
+document.getElementsByTagName
+
+document.querySelector
+document.querySelectorAll
+
+
+document.getElementById("demo").innerHTML = "Milla"
+const bush = document.getElementsByClassName("mean")
+console.log(bush)
+
+const arrayExtract = Array.from(bush)
+
+console.log(arrayExtract)
+
+arrayExtract.forEach((element)=>{})
+
+
+paragraph.innerHTML = "Hello World"
